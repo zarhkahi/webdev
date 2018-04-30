cfb3f6fe85ad1833fb4e489a3758bacc8b2e437b
Zarh <zarhkahi@gmail.com>
2018-05-01 01:59:20 +0200

    view contenido

--- html/contenido.php
+++ html/contenido.php
@@ -5,12 +5,45 @@ require_once __DIR__.'/includes/config.php';
 use es\ucm\fdi\aw;
 
 function mostrarContenido() {
-  $html = '';
-  $app = aw\Aplicacion::getSingleton();
+	$html = 'Lista de eventos: ' . "\n";
+  	$app = aw\Aplicacion::getSingleton();
 	if ($app->usuarioLogueado()) {
-		$html = \es\ucm\fdi\aw\Evento::showUserEvents($app->idUsuario());  
-	} else {
-		$html = \es\ucm\fdi\aw\Evento::showAllEvents();
+		$events = \es\ucm\fdi\aw\Evento::userEvents($app->idUsuario()); 
+		if($events){
+		  foreach ($events as $item => $event){
+			$html .= "<div class=\"eventCont\">";
+			$html .= '<div><img src="/AW/includes/fotos-eventos/37.jpg"></div>';
+			//$html .= "<1h>" . $item->nombre_evento() . "</1h> \n" . "<p>" . $item->fecha_evento() . " "  . $item->precio_evento() . "</p> \n";
+			$html .= "<div><h1>" . $event['nombre'] . "</h1>" . "<p> Fecha:" . $event['fecha']  . "</p></div>";
+			$html .=  '<div><p><form method="POST" action="updateEvento.php" class="null" enctype="">
+        	<input class="null" name="id_e" value="'. $event['id_evento'] . '" type="hidden" readonly>	
+        	<button class="buttonCont" type="submit"><span class="button__inner">Edit</span></button>
+        	</form></p>';
+        	$html .=  '<p><form method="POST" action="deleteEvento.php" class="null" enctype="">
+        	<input class="null" name="id_e" value="'. $event['id_evento'] . '" type="hidden" readonly>	
+        	<button class="buttonCont buttonCont--secondary" type="submit"><span class="button__inner">Delete</span></button>
+        	</form></p></div>';
+			$html .= "</div>";
+		  }
+		}
+		else{
+		  $html .= "No hay eventos.";
+		}
+		return $html;
+
+	} else { //No!! Should be index!
+		$events = \es\ucm\fdi\aw\Evento::allEvents();
+		if($events){
+			foreach ($events as $item => $event){
+			  $html .= "<div class=\"eventCont\">";
+			  $html .= '<div><img src="/AW/includes/fotos-eventos/37.jpg"></div>';
+			  $html .= "<div><h1>" . $event['nombre'] . "</h1>" . "<p> Fecha:" . $event['fecha']  . "</p></div>";
+			  $html .= "</div>";
+			}
+		  }
+		else{
+			$html .= "No hay eventos.";
+		}
   }
 
   return $html;
