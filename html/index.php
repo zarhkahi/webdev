0962c5c61e6643e99ecc5ef7a457ce07ece73918
KestrelSoftworks <37401928+KestrelSoftworks@users.noreply.github.com>
2018-04-30 14:12:46 +0200

    comentario en php de html responsive

--- html/index.php
+++ html/index.php
@@ -1,5 +1,17 @@
 <?php
 
+/*<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<style>
+img {
+    max-width: 100%;
+    height: auto;
+}
+</style>
+</head>
+*/
 require_once __DIR__.'/includes/config.php';
 
 use es\ucm\fdi\aw;
